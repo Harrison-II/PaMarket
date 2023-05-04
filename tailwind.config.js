0edb2c25bdff707c0b67659dd7e583b5ee7b16f3
Ndeji <ndejichiputa@gmail.com>
2023-05-04 17:32:22 +0200

    Fifth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,14 @@ module.exports = {
       'noto': ['Noto Serif', 'serif'],
       'quicksand': ['Quicksand', 'sans-serif'],
       'arvo': ['Arvo', 'serif'],
+      'inter': ['Inter', 'sans-serif'],
+      'nunito': ['Nunito Sans', 'sans-serif'],
     },
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedDark: 'hsl(12, 62%, 46%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
