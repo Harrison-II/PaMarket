97f9c3cdf6a1acbda3d544e6ee95e54b7c6b2793
Ndeji <ndejichiputa@gmail.com>
2023-05-30 11:50:37 +0200

    Committing for review

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,7 +18,7 @@ window.addEventListener("scroll", () => {
       nav_bg.classList.remove("bg-transparent");
       nav_bg.classList.add("opacity-50")
     }
-  } else if(!nav_bg.classList.contains("bg-transparent")) {
+  } else if (!nav_bg.classList.contains("bg-transparent")) {
     if (window.scrollY === 0) {
       nav_bg.classList.add("bg-transparent")
       nav_bg.classList.remove("opacity-50")
@@ -29,3 +29,44 @@ window.addEventListener("scroll", () => {
   }
 });
 
+function sendContactMail() {
+  var params = {
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_rtwnsn9";
+  const templateID = "template_slzo1on";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      location.reload()
+    })
+    .catch(err => console.log(err));
+
+}
+
+function sendSubscribeMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    mobile: document.getElementById("mobile").value,
+  };
+
+  const serviceID = "service_rtwnsn9";
+  const templateID = "template_kr4qebc";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("mobile").value = "";
+      location.reload()
+    })
+    .catch(err => console.log(err));
+
+}
