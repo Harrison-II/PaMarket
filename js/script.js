d848964f52222bbbfb7c042eabb3fd4ed1da3653
Ndeji <ndejichiputa@gmail.com>
2023-08-01 15:16:42 +0200

    Added new layout to articles page

--- js/script.js
+++ js/script.js
@@ -108,3 +108,26 @@ function sendSubscribeMail() {
     );
 
 }
+
+function changeTab(evt, article) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" border-brightRed", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(article).style.display = "block";
+  evt.currentTarget.className += " active";
+  evt.currentTarget.className += " border-brightRed";
+} 
